43da80818b29ff42fabd9dabd352f9a81f2f78cc
Adeel Ahmed Khan <adeelbarki@gmail.com>
2019-11-13 21:18:21 +0500

    Stopping and starting ec2 instance on scheduled time

---
+++ lambda_function_start.py
@@ -0,0 +1,20 @@
+import boto3
+
+def lambda_handler(event, context):
+    
+    ec2_client = boto3.client('ec2')
+    regions = [region['RegionName']
+        for region in ec2_client.describe_regions()['Regions']]
+        
+    for region in regions:
+        ec2 = boto3.resource('ec2', region_name = region)
+        print('Regions: ', region)
+        
+        instances = ec2.instances.filter(
+            Filters = [{'Name': 'instance-state-name',
+                        'Values': ['stopped']
+            }])
+            
+        for instance in instances:
+            instance.start()
+            print('Stopped Instance: ', instance.id)
